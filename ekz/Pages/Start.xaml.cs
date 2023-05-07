966348b173dc10fbf3defc1a95362af84495141c
Макс Лик <maxlikarenko@gmail.com>
2023-05-07 19:08:54 +0200

    Added export data into CSV file

--- ekz/Pages/Start.xaml.cs
+++ ekz/Pages/Start.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +33,33 @@ namespace ekz.Pages {
 
 		private void Students_Click(object sender, RoutedEventArgs e) {
 			NavigationService.Navigate(new StudentsMain());
+		}
+
+		private void ExportCSV_Click(object sender, RoutedEventArgs e) {
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Filter = "Файл CSV|*.csv";
+			if (saveFileDialog.ShowDialog() == true) {
+				using (var writer = new StreamWriter(saveFileDialog.FileName, false, Encoding.UTF8)) {
+					var csvConfig = new CsvConfiguration(CultureInfo.GetCultureInfo("ua-UA")) {
+						HasHeaderRecord = false,
+						Delimiter = ";"
+					};
+					using (var csv = new CsvWriter(writer, csvConfig)) {
+						try {
+							csv.WriteRecords(CourseRepository.GetCourses());
+							csv.WriteRecords(StudentRepository.GetStudents());
+							MessageBox.Show("Дані успішно експортовано");
+						}
+						catch (ConfigurationException ex) {
+							MessageBox.Show("Помилка збереження!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+						}
+					}
+				}
+			}
+        }
+
+		private void ExportExcel_Click(object sender, RoutedEventArgs e) {
+
 		}
 	}
 }
