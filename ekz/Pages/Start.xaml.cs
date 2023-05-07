033e678a54e3682260b74a1fde5f66ca7f33d3df
Макс Лик <maxlikarenko@gmail.com>
2023-05-07 21:03:09 +0200

    Added export data into Excel file (.xlsx)

--- ekz/Pages/Start.xaml.cs
+++ ekz/Pages/Start.xaml.cs
@@ -1,5 +1,7 @@
-﻿using CsvHelper;
+﻿using ClosedXML.Excel;
+using CsvHelper;
 using CsvHelper.Configuration;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -22,7 +24,7 @@ namespace ekz.Pages {
 	/// <summary>
 	/// Логика взаимодействия для Start.xaml
 	/// </summary>
-	public partial class Start : Page {
+	public partial class Start : System.Windows.Controls.Page {
 		public Start() {
 			InitializeComponent();
 		}
@@ -59,7 +61,41 @@ namespace ekz.Pages {
         }
 
 		private void ExportExcel_Click(object sender, RoutedEventArgs e) {
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Filter = "Книга Excel|*.xlsx";
+			if (saveFileDialog.ShowDialog() == true) {
+				try {
+					var workbook = new XLWorkbook();
+					var sheetCourses = workbook.Worksheets.Add("Courses");
+					var courses = CourseRepository.GetCourses();
+					sheetCourses.Cell(1, 1).SetValue("Id");
+					sheetCourses.Cell(1, 2).SetValue("Курс");
+					sheetCourses.Cell(1, 3).SetValue("Викладач");
+					for (int i = 2; i < courses.Count + 2; i++) {
+						sheetCourses.Cell(i, 1).SetValue(courses[i - 2].Id);
+						sheetCourses.Cell(i, 2).SetValue(courses[i - 2].Name);
+						sheetCourses.Cell(i, 3).SetValue(courses[i - 2].Teacher);
+					}
 
+					var sheetStudents = workbook.Worksheets.Add("Students");
+					var students = StudentRepository.GetStudents();
+					sheetStudents.Cell(1, 1).SetValue("Id");
+					sheetStudents.Cell(1, 2).SetValue("Ім'я студента");
+					sheetStudents.Cell(1, 3).SetValue("Прізвище студента");
+					sheetStudents.Cell(1, 4).SetValue("Вік студента");
+					for (int i = 2; i < students.Count + 2; i++) {
+						sheetStudents.Cell(i, 1).SetValue(students[i - 2].Id);
+						sheetStudents.Cell(i, 2).SetValue(students[i - 2].Name);
+						sheetStudents.Cell(i, 3).SetValue(students[i - 2].Surname);
+						sheetStudents.Cell(i, 4).SetValue(students[i - 2].Age);
+					}
+					workbook.SaveAs(saveFileDialog.FileName);
+					MessageBox.Show("Дані успішно експортовано");
+				}
+				catch {
+					MessageBox.Show("Помилка збереження!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+			}
 		}
 	}
 }
